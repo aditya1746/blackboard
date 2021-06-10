f70c60bcb908085d1f1482774f0d5b86026f9651
aditya1746 <adityarajsinghgour17@gmail.com>
2021-06-10 16:19:08 +0530

    changed interface, integrated pen, colorfill, save

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ now = dt.now()
 #dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
 #print("date and time =", dt_string)
 drawing = False
-erase = False
+colorfill = False
 prev_point = (0,0)
 blue = (255,0,0)
 green = (0,255,0)
@@ -22,7 +22,7 @@ font = cv2.FONT_HERSHEY_COMPLEX
 
 def draw (event,x,y,flags, param):
 
-    global drawing,color,mode,erase
+    global drawing,color,mode,colorfill
     global prev_point
 
     if event == cv2.EVENT_LBUTTONDOWN and (y>45 and y<467) and mode=="pen":
@@ -34,12 +34,12 @@ def draw (event,x,y,flags, param):
 
         prev_point = (x,y)
     
-    elif event == cv2.EVENT_LBUTTONDOWN and (y>45 and y<467) and mode=="erase":
+    elif event == cv2.EVENT_LBUTTONDOWN and (y>45 and y<467) and mode=="color fill":
 
-        erase = True
+        colorfill = True
         print("left button down")
 
-        cv2.circle(img,(x,y),10,(0,0,0),-1)
+        cv2.circle(img,(x,y),10,color,-1)
 
         prev_point = (x,y)
 
@@ -53,11 +53,11 @@ def draw (event,x,y,flags, param):
 
         print("mouse move")
 
-    elif event == cv2.EVENT_MOUSEMOVE and erase == True and (y>45 and y<467) and mode=="erase":
+    elif event == cv2.EVENT_MOUSEMOVE and colorfill == True and (y>45 and y<467) and mode=="color fill":
 
         #cv2.circle(img,(x,y),5,(0,0,0),-1)
 
-        cv2.line(img,prev_point,(x,y),(0,0,0),10)
+        cv2.line(img,prev_point,(x,y),color,10)
 
         prev_point = (x,y)
 
@@ -67,7 +67,7 @@ def draw (event,x,y,flags, param):
 
         print("mouse up")
         drawing = False
-        erase = False
+        colorfill = False
 
     elif event == cv2.EVENT_LBUTTONDBLCLK:
 
@@ -97,6 +97,13 @@ def draw (event,x,y,flags, param):
                 cv2.rectangle(img,(316,5),(392,35),(0,0,0),1)
                 cv2.rectangle(img,(414,5),(490,35),(0,0,0),1)
                 cv2.rectangle(img,(218,5),(294,35),(255,255,255),1)
+                cv2.waitKey(500)
+                dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
+                dt_string = dt_string + ".png"
+                cv2.imwrite(dt_string,img)
+                dt_string = "saved as "+dt_string
+                cv2.imshow(dt_string,img)
+                
             if(x>=316 and x<=392):
                 color = (0,0,255)
                 cv2.rectangle(img,(22,5),(98,35),(0,0,0),1)
@@ -122,39 +129,86 @@ def draw (event,x,y,flags, param):
             if x>=10 and x<=40 and y>=477 and y<=507:
                 color = black
                 cv2.circle(img,(25,492),15,black,-1)
-                cv2.circle(img,(25,492),15,white,3)
+                cv2.circle(img,(25,492),15,blue,-1)
+                cv2.circle(img,(25,492),12,black,-1)
                 cv2.circle(img,(60,492),15,white,-1)
                 cv2.circle(img,(95,492),15,red,-1)
                 cv2.circle(img,(130,492),15,green,-1)
-                cv2.circle(img,(165,492),15,blue,-1)
-                cv2.circle(img,(200,492),15,yellow,-1)
-                cv2.circle(img,(235,492),15,skyblue,-1)
-                cv2.circle(img,(270,492),15,pink,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
 
             if x>=45 and x<=75 and y>=477 and y<=507:
                 color = white
                 cv2.circle(img,(25,492),15,black,-1)
                 cv2.circle(img,(60,492),15,white,-1)
-                cv2.circle(img,(60,492),15,black,3)
+                cv2.circle(img,(60,492),15,blue,-1)
+                cv2.circle(img,(60,492),12,white,-1)
                 cv2.circle(img,(95,492),15,red,-1)
                 cv2.circle(img,(130,492),15,green,-1)
-                cv2.circle(img,(165,492),15,blue,-1)
-                cv2.circle(img,(200,492),15,yellow,-1)
-                cv2.circle(img,(235,492),15,skyblue,-1)
-                cv2.circle(img,(270,492),15,pink,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
 
             if x>=80 and x<=110 and y>=477 and y<=507:
                 color = red
                 cv2.circle(img,(25,492),15,black,-1)
                 cv2.circle(img,(60,492),15,white,-1)
                 cv2.circle(img,(95,492),15,red,-1)
-                cv2.circle(img,(95,492),15,black,3)
+                cv2.circle(img,(95,492),15,blue,-1)
+                cv2.circle(img,(95,492),12,red,-1)
                 cv2.circle(img,(130,492),15,green,-1)
-                cv2.circle(img,(165,492),15,blue,-1)
-                cv2.circle(img,(200,492),15,yellow,-1)
-                cv2.circle(img,(235,492),15,skyblue,-1)
-                cv2.circle(img,(270,492),15,pink,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
 
+            if x>=115 and x<=145 and y>=477 and y<=507:
+                color = green
+                cv2.circle(img,(25,492),15,black,-1)
+                cv2.circle(img,(60,492),15,white,-1)
+                cv2.circle(img,(95,492),15,red,-1)
+                cv2.circle(img,(130,492),15,green,-1)
+                cv2.circle(img,(130,492),15,blue,-1)
+                cv2.circle(img,(130,492),12,green,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
+
+            if x>=150 and x<=180 and y>=477 and y<=507:
+                color = yellow
+                cv2.circle(img,(25,492),15,black,-1)
+                cv2.circle(img,(60,492),15,white,-1)
+                cv2.circle(img,(95,492),15,red,-1)
+                cv2.circle(img,(130,492),15,green,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(165,492),15,blue,-1)
+                cv2.circle(img,(165,492),12,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
+
+            if x>=185 and x<=215 and y>=477 and y<=507:
+                color = skyblue
+                cv2.circle(img,(25,492),15,black,-1)
+                cv2.circle(img,(60,492),15,white,-1)
+                cv2.circle(img,(95,492),15,red,-1)
+                cv2.circle(img,(130,492),15,green,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(200,492),15,blue,-1)
+                cv2.circle(img,(200,492),12,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
+
+            if x>=220 and x<=250 and y>=477 and y<=507:
+                color = pink
+                cv2.circle(img,(25,492),15,black,-1)
+                cv2.circle(img,(60,492),15,white,-1)
+                cv2.circle(img,(95,492),15,red,-1)
+                cv2.circle(img,(130,492),15,green,-1)
+                cv2.circle(img,(165,492),15,yellow,-1)
+                cv2.circle(img,(200,492),15,skyblue,-1)
+                cv2.circle(img,(235,492),15,pink,-1)
+                cv2.circle(img,(235,492),15,blue,-1)
+                cv2.circle(img,(235,492),12,pink,-1)
 
 img = np.zeros((512,512,3), np.uint8)
 
@@ -179,11 +233,11 @@ cv2.circle(img,(25,492),15,black,-1)
 cv2.circle(img,(60,492),15,white,-1)
 cv2.circle(img,(95,492),15,red,-1)
 cv2.circle(img,(130,492),15,green,-1)
-cv2.circle(img,(130,492),15,black,3)
-cv2.circle(img,(165,492),15,blue,-1)
-cv2.circle(img,(200,492),15,yellow,-1)
-cv2.circle(img,(235,492),15,skyblue,-1)
-cv2.circle(img,(270,492),15,pink,-1)
+cv2.circle(img,(130,492),15,blue,-1)
+cv2.circle(img,(130,492),12,green,-1)
+cv2.circle(img,(165,492),15,yellow,-1)
+cv2.circle(img,(200,492),15,skyblue,-1)
+cv2.circle(img,(235,492),15,pink,-1)
 cv2.rectangle(img,(472,477),(502,507),(0,0,0),1)
 cv2.putText(img,'X',(477,503),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),1,cv2.LINE_AA)
 
